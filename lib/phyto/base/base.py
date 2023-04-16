ddf5a8abe790e7496b8662e58ce660444d331e8b
Austin Bowen <austin.bowen.314@gmail.com>
2023-04-16 15:28:53 -0400

    Add rest() and stand() methods

--- lib/phyto/base/base.py
+++ lib/phyto/base/base.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+import time
 from typing import Sequence
 
 from phyto.base.leg import Leg, get_legs
@@ -12,12 +12,42 @@ def get_base(
     return Base(left_legs, right_legs)
 
 
-@dataclass
 class Base:
     left_legs: Sequence[Leg]
     right_legs: Sequence[Leg]
 
+    leg_groups: Sequence[Sequence[Leg]]
+
+    def __init__(self, left_legs: Sequence[Leg], right_legs: Sequence[Leg]) -> None:
+        self.left_legs = left_legs
+        self.right_legs = right_legs
+
+        self.leg_groups = [
+            (left_legs[0], right_legs[1], left_legs[2]),
+            (right_legs[0], left_legs[1], right_legs[2]),
+        ]
+
+    @property
+    def legs(self) -> Sequence[Leg]:
+        return tuple(*self.left_legs, *self.right_legs)
+
+    def rest(self, speed: float) -> None:
+        for leg in self.legs:
+            leg.rest(speed)
+
+        # TODO
+        print(f'rest: speed={speed}')
+        time.sleep(1)
+
+    def stand(self, speed: float, height: float) -> None:
+        for leg in self.legs:
+            leg.stand(speed, height)
+
+        # TODO
+        print(f'stand: speed={speed}, height={height}')
+        time.sleep(1)
+
     def step(self, speed: float, direction: float) -> None:
         # TODO
         print(f'step: speed={speed}, direction={direction}')
-        import time; time.sleep(1)
+        time.sleep(1)
--- lib/phyto/base/leg.py
+++ lib/phyto/base/leg.py
@@ -1,8 +1,10 @@
-from typing import Sequence, Tuple
+from typing import Sequence, Tuple, Optional
 
 from adafruit_motor.servo import Servo
 
 from phyto.base.servo_controller import ServoController
+
+ServoAngle = Optional[float]
 
 
 def get_legs(
@@ -25,44 +27,76 @@ def get_legs(
 
 def get_left_front_leg(servo_controller: ServoController) -> 'Leg':
     return Leg(
+        id='left_front',
         servos=servo_controller.get_servos(22, 23, 27),
     )
 
 
 def get_left_middle_leg(servo_controller: ServoController) -> 'Leg':
     return Leg(
+        id='left_middle',
         servos=servo_controller.get_servos(19, 20, 21),
     )
 
 
 def get_left_back_leg(servo_controller: ServoController) -> 'Leg':
     return Leg(
+        id='left_back',
         servos=servo_controller.get_servos(16, 17, 18),
     )
 
 
 def get_right_front_leg(servo_controller: ServoController) -> 'Leg':
     return Leg(
+        id='right_front',
         servos=servo_controller.get_servos(9, 8, 31),
     )
 
 
 def get_right_middle_leg(servo_controller: ServoController) -> 'Leg':
     return Leg(
+        id='right_middle',
         servos=servo_controller.get_servos(12, 11, 10),
     )
 
 
 def get_right_back_leg(servo_controller: ServoController) -> 'Leg':
     return Leg(
+        id='right_back',
         servos=servo_controller.get_servos(15, 14, 13),
     )
 
 
 class Leg:
+    id: str
+
     servos: Sequence[Servo]
     """Three servos in order from internal to external joints."""
 
-    def __init__(self, servos: Sequence[Servo]) -> None:
+    def __init__(self, id: str, servos: Sequence[Servo]) -> None:
         assert len(servos) == 3, len(servos)
+
+        self.id = id
         self.servos = servos
+
+    def __repr__(self) -> str:
+        return f'Leg(id={self.id!r})'
+
+    @property
+    def angles(self) -> Tuple[ServoAngle, ...]:
+        return tuple(servo.angle for servo in self.servos)
+
+    @angles.setter
+    def angles(self, angles: Tuple[ServoAngle, ...]) -> None:
+        assert len(angles) == 3, len(angles)
+
+        for servo, angle in zip(self.servos, angles):
+            servo.angle = angle
+
+    def rest(self, speed: float) -> None:
+        # TODO
+        ...
+
+    def stand(self, speed: float, height: float) -> None:
+        # TODO
+        ...
