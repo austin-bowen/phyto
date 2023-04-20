53147669ceee67ddd7bd8c359decdf0fb6ad37b5
Austin Bowen <austin.bowen.314@gmail.com>
2023-04-19 23:52:54 -0400

    Get i2c working

--- phyto/config.py
+++ phyto/config.py
@@ -2,8 +2,8 @@ import board
 
 from phyto.types import I2cAddress, Pin
 
-I2C_BUS_SDA: Pin = board.SDA
 I2C_BUS_SCL: Pin = board.SCL
+I2C_BUS_SDA: Pin = board.SDA
 I2C_BUS_FREQ: int = 100000
 
 PCA9685_0_I2C_ADDRESS: I2cAddress = 0x40
--- phyto/main.py
+++ phyto/main.py
@@ -1,19 +1,22 @@
+from time import sleep
+
+from busio import I2C
+
+from phyto import config
 from phyto.base import get_base
 from phyto.base.servo_controller import get_servo_controller
 from phyto.types import I2cAddress, Pin
-from phyto import config
-from busio import I2C
 
 
 def main(
-        i2c_bus_sda: Pin = config.I2C_BUS_SDA,
         i2c_bus_scl: Pin = config.I2C_BUS_SCL,
+        i2c_bus_sda: Pin = config.I2C_BUS_SDA,
         i2c_bus_freq: int = config.I2C_BUS_FREQ,
         pca9685_0_i2c_address: I2cAddress = config.PCA9685_0_I2C_ADDRESS,
         pca9685_1_i2c_address: I2cAddress = config.PCA9685_1_I2C_ADDRESS,
         pca9685_pwm_freq: int = config.PCA9685_PWM_FREQ,
 ):
-    i2c_bus = I2C(i2c_bus_sda, i2c_bus_scl, frequency=i2c_bus_freq)
+    i2c_bus = get_i2c_bus(i2c_bus_scl, i2c_bus_sda, i2c_bus_freq)
 
     servo_controller = get_servo_controller(
         i2c_bus,
@@ -26,3 +29,13 @@ def main(
 
     while True:
         base.step(speed=1., direction=0.)
+
+
+def get_i2c_bus(scl: Pin, sda: Pin, freq: int):
+    while True:
+        try:
+            return I2C(scl, sda, frequency=freq)
+        except RuntimeError as e:
+            print(f'Error creating I2C bus; error={e}')
+            print('Retrying...')
+            sleep(1)
