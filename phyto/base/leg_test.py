8595d36b8903fb97d7d7472ef90a4c047628cdc5
Austin Bowen <austin.bowen.314@gmail.com>
2023-08-13 15:24:52 -0400

    Allow changing what leg(s) to control

--- phyto/base/leg_test.py
+++ phyto/base/leg_test.py
@@ -1,7 +1,8 @@
 import math
 
 from phyto import config
-from phyto.base.leg import get_right_front_leg, get_right_middle_leg, get_right_back_leg
+from phyto.base.leg import get_right_front_leg, get_right_middle_leg, get_right_back_leg, get_left_front_leg, \
+    get_left_middle_leg, get_left_back_leg
 from phyto.base.servo_controller import get_servo_controller, ServoController
 from phyto.i2c import get_i2c_bus
 from phyto.kinematics.inverse import InverseSolver3Dof, NoSolution
@@ -30,15 +31,28 @@ def leg_test(
 
 
 def run(servo_controller: ServoController) -> None:
-    # leg = get_right_front_leg(servo_controller)
-    leg = get_right_middle_leg(servo_controller)
-    # leg = get_right_back_leg(servo_controller)
+    legs = dict(
+        lf=get_left_front_leg(servo_controller),
+        lm=get_left_middle_leg(servo_controller),
+        lb=get_left_back_leg(servo_controller),
+        rf=get_right_front_leg(servo_controller),
+        rm=get_right_middle_leg(servo_controller),
+        rb=get_right_back_leg(servo_controller),
+    )
+
+    leg_name = 'all'
 
     solver = InverseSolver3Dof(l0=.032, l1=.090, l2=.112)
 
     while True:
         try:
-            command = input('x,y,z: ')
+            print(f'Leg: {leg_name}')
+            command = input('"<l|r><f|m|b>" OR "all" OR x,y,z: ')
+
+            if command in legs or command == 'all':
+                leg_name = command
+                continue
+
             x, y, z = map(float, command.split(','))
 
             theta0, theta1, theta2 = solver.solve(x, y, z)
@@ -55,7 +69,8 @@ def run(servo_controller: ServoController) -> None:
 
             print(f'Offset: theta0={theta0}, theta1={theta1}, theta2={theta2}')
 
-            leg.angles = (theta0, theta1, theta2)
+            for leg in legs.values() if leg_name == 'all' else [legs[leg_name]]:
+                leg.angles = (theta0, theta1, theta2)
         except KeyboardInterrupt:
             break
         except Exception as e:
